2323eb9471c3ae5628674191a3037e727fd8e302
Guido Martínez <mtzguido@gmail.com>
2023-04-20 21:00:33 -0700

    blah

--- ocaml/fstar/main.ml
+++ ocaml/fstar/main.ml
@@ -1,4 +1,5 @@
 let x =
       Printexc.record_backtrace true;
       Gc.set { (Gc.get()) with Gc.minor_heap_size = 1048576; Gc.major_heap_increment = 4194304; Gc.space_overhead = 150; };
+      Gc.set { (Gc.get()) with Gc.max_overhead = 0 };
       FStar_Main.main ()
