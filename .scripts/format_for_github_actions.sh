cf0b414c4f6ddc2126bfa8eb9f2352c74c9b9ca7
Guido Martínez <mtzguido@gmail.com>
2024-10-09 20:14:41 -0700

    update filter

--- .scripts/format_for_github_actions.sh
+++ .scripts/format_for_github_actions.sh
@@ -2,21 +2,26 @@
 
 set -eu
 
-while read msg; do
-  echo "$msg" # Always repeat everything
-  if ! echo "$msg" | grep -q '^{'; then
-    continue
-  fi
-  level=$(jq .level <<< $msg)
+# Call this with no arguments and feeding a build log to stdin,
+# or with arguments that are filenames to buildogs.
+
+grep '^{' "$@" | while IFS= read -r msg; do
+  echo "$msg" # We always repeat everything back
+  level=$(jq -r .level <<< $msg)
   file=$(jq -r .range.def.file_name <<< $msg)
-  file=$(realpath --relative-to=. "${file}")
-  line=$(jq .range.def.start_pos.line <<< $msg)
-  endLine=$(jq .range.def.end_pos.line <<< $msg)
-  body=$(cat <<< $msg | jq '.msg | join (". ")' | tr '\n' ' ')
+  # -m: do not fail if file does not exist
+  file=$(realpath -m --relative-to=. "${file}")
+  line=$(jq -r .range.def.start_pos.line <<< $msg)
+  endLine=$(jq -r .range.def.end_pos.line <<< $msg)
+  body=$(cat <<< "$msg" | jq -r '.msg | join ("; ")')
+  body=$(tr '\n' ' ' <<< $body)
   if [ "${level}" == "Error" ]; then
     glevel=error
   elif [ "${level}" == "Warning" ]; then
     glevel=warning
+  else
+    # Info message, probably
+    continue
   fi
   echo "::${glevel} file=$file,line=$line,endLine=$endLine::$body"
 done
