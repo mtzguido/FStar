4e18be161563a8a0caa2b5dbea0fdc325b5d38ac
Guido Martínez <mtzguido@gmail.com>
2026-03-20 19:24:41 -0700

    try not doing anything

--- src/ml/FStarC_GCSetup.ml
+++ src/ml/FStarC_GCSetup.ml
@@ -1,5 +1,1 @@
-let setup () =
-  Gc.set { (Gc.get()) with
-    Gc.minor_heap_size = 1048576;  (* 8 MiB, 4x the default *)
-    Gc.space_overhead = 80;        (* collect more aggressively than the default 120 *)
-  }
+let setup () = ()
